14a4c38036984b243f7f208d26799a0092a4433e
Surya Rosauli Pasaribu <93670448+asterisros@users.noreply.github.com>
2022-03-10 21:30:35 +0700

    Add files via upload

---
+++ Submission EDU.io/assets/js/button.js
@@ -0,0 +1,9 @@
+const downloadApps = document.querySelector(".btn");
+downloadApps.addEventListener("click", function() {
+    alert("Your download will be started");
+})
+
+const submitForm = document.getElementById("submit");
+submitForm.addEventListener("click", function() {
+    alert("Click OK to sent messages!");
+})
